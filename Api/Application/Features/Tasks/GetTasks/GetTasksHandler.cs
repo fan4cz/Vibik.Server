db3a7ab4655661022ef60ff8ccc5d229bf15b52e
Lychee <2006tka@gmail.com>
2025-11-21 11:22:18 +0500

    ох уж это форматирование...

--- Api/Application/Features/Tasks/GetTasks/GetTasksHandler.cs
+++ Api/Application/Features/Tasks/GetTasks/GetTasksHandler.cs
@@ -1,15 +1,15 @@
-﻿    using Infrastructure.Interfaces;
-    using MediatR;
-    using Shared.Models;
+﻿using Infrastructure.Interfaces;
+using MediatR;
+using Shared.Models;
 
-    namespace Api.Application.Features.Tasks.GetTasks;
+namespace Api.Application.Features.Tasks.GetTasks;
 
-    public class GetTasksHandler(IUsersTasksTable tasks) : IRequestHandler<GetTasksQuery, List<TaskModel>>
+public class GetTasksHandler(IUsersTasksTable tasks) : IRequestHandler<GetTasksQuery, List<TaskModel>>
+{
+    public async Task<List<TaskModel>> Handle(GetTasksQuery request,
+        CancellationToken cancellationToken)
     {
-        public async Task<List<TaskModel>> Handle(GetTasksQuery request,
-            CancellationToken cancellationToken)
-        {
-            var tasksList = await tasks.GetListActiveUserTasks(request.Username);
-            return tasksList;
-        }
+        var tasksList = await tasks.GetListActiveUserTasks(request.Username);
+        return tasksList;
     }
+}
