ba30e42cc3032d7d5fb8be5df9665e4950a982c2
fan4cz <fan4cz@gmail.com>
2025-12-06 13:51:05 +0500

    qwerty

--- Api/Application/Features/Tasks/ChangeTask/ChangeTaskHandler.cs
+++ Api/Application/Features/Tasks/ChangeTask/ChangeTaskHandler.cs
@@ -1,18 +1,19 @@
 ﻿using Infrastructure.Interfaces;
 using MediatR;
+using Shared.Models.Entities;
 using Shared.Models.Enums;
 
 namespace Api.Application.Features.Tasks.ChangeTask;
 
 // TODO: вместо bool как будто бы хочется TaskModel возвращать
-public class ChangeTaskHandler(IUsersTasksTable tasks, IMetricsTable metrics) : IRequestHandler<ChangeTaskQuery, bool>
+public class ChangeTaskHandler(IUsersTasksTable tasks, IMetricsTable metrics) : IRequestHandler<ChangeTaskQuery, TaskModel>
 {
-        public async Task<bool> Handle(ChangeTaskQuery request, CancellationToken cancellationToken)
+        public async Task<TaskModel> Handle(ChangeTaskQuery request, CancellationToken cancellationToken)
         {
                 var username = request.Username;
 
                 var newTask = await tasks.AddUserTask(username);
                 metrics.AddRecord(MetricType.Change);
-                return newTask is not null;
+                return newTask;
         }
 }
