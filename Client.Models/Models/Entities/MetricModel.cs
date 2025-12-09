743a5378604175da6cf6ac399a271defced84f5c
kitiketov <roskets801@gmail.com>
2025-12-09 14:01:13 +0500

    хз работает или нет

--- Client.Models/Models/Entities/MetricModel.cs
+++ Client.Models/Models/Entities/MetricModel.cs
@@ -1,6 +1,12 @@
+using Shared.Models.Enums;
+
 namespace Shared.Models.Entities;
 
 public class MetricModel
 {
-    
+    public int Id;
+    public string Username;
+    public MetricType Type;
+    public TimeSpan Time;
+
 }
